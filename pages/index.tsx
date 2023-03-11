604ee56ee5032c82bd67c218a9e6575b493aa77f
vipulpathak113 <vipulpathak113@gmail.com>
2023-03-11 23:58:01 +0530

    styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,38 +3,38 @@ import type { ReactElement } from "react";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-center m-10">
+      <h1 className="text-center m-10 font-[cursive] text-3xl">
         Top 10 Accessibility Best Practices For Designers
       </h1>
 
       <div className="grid grid-cols-[400px_400px] justify-center">
         <div>
           <span>1. </span>
-          <Link href="/colorContrast" className="text-blue-600 underline">
+          <Link href="/colorContrast" className="text-blue-600 underline font-[fantasy]">
             Color Contrast
           </Link>
         </div>
         <div>
           <span>2. </span>
-          <Link href="/colorIndependence" className="text-blue-600 underline">
+          <Link href="/colorIndependence" className="text-blue-600 underline font-[fantasy] font-[fantasy]">
             Color Independence
           </Link>
         </div>
         <div>
           <span>3. </span>
-          <Link href="/wordingInteractive" className="text-blue-600 underline">
+          <Link href="/wordingInteractive" className="text-blue-600 underline font-[fantasy]">
             Wording Interactive Elements
           </Link>
         </div>
         <div>
           <span>4. </span>
-          <Link href="/stylingInteractive1" className="text-blue-600 underline">
+          <Link href="/stylingInteractive1" className="text-blue-600 underline font-[fantasy]">
             Styling Interactive Elements (Part1: Buttons and Like)
           </Link>
         </div>
         <div>
           <span>5. </span>
-          <Link href="/stylingInteractive2" className="text-blue-600 underline">
+          <Link href="/stylingInteractive2" className="text-blue-600 underline font-[fantasy]">
             Styling Interactive Elements (Part2: Links and Like)
           </Link>
         </div>
